57b908c542c1b84fd6d101b69b61f930bbca4adc
Sn0wie <67165527+piggydoe@users.noreply.github.com>
2023-06-25 12:29:23 -0700

    2

---
+++ src/components/Item.svelte
@@ -0,0 +1,49 @@
+<script>
+    // @ts-ignore
+    export let itemID;
+    // @ts-ignore
+    export let title;
+
+    export let price;
+
+    import { discordUser } from '../store/store'
+
+    function sendWebhook(webHookURL, message) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", webHookURL, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+            'content': message,
+            'username':'AI',
+        }));
+    }
+
+    const order = () => {
+        sendWebhook("https://discord.com/api/webhooks/1122584457602400286/9ORmcF9lK-Q4Z1SSnoDvxhNDM7jpP_Z3DlI05hnvO-PZgbPVZy6VfKzsxpDJRcTAdHE6", `${$discordUser} ORDERED PRODUCT ID: ${itemID}`)
+        alert("MESSAGE FROM S&M: Our staff will get back to you as soon as possible\nA DM will be sent.")
+    }
+</script>
+
+<div class="card">
+    <h1>{title}</h1>
+    <p>{price}</p>
+    <button class="btn" on:click={order}>ORDER - ID: {itemID}</button>
+</div>
+
+<style lang="css">
+.card{
+    background:#2c3e50;
+    padding: 20px;
+    border-radius: 10px;
+    width: 70%;
+}
+.btn{
+    border:none;
+    color: #ecf0f1;
+    padding:10px;
+    border-radius:5px;
+    width: 100%;
+    cursor: pointer;
+    background: #2ecc71;
+}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+import Item from "../components/Item.svelte";
+import { discordUser } from '../store/store'
+
+let isModalShown = false;
+let ID = "Discord Tag";
+const validateID = () => {
+    console.log(ID)
+    discordUser.set(ID)
+    isModalShown = true
+}
+const showIdModal = () => {
+}
+</script>
+
+{#if isModalShown != true}
+<div class="login">
+    <input class="login-field" bind:value={ID} placeholder="enter tag or username"/>
+    <button class="btn red" on:click={validateID}>Enter</button>
+    <p>Disclaimer: Failing to provide a valid discord tag/username can result in (us) refusing service.</p>
+</div>
+{:else}
+<main>
+    <div class="nav">
+        <div class="inner-nav">
+            <h1>Welcome {$discordUser}</h1>
+            <button on:click={() => location.reload(true)} class="btn red">Logout</button>
+        </div>
+    </div>
+    <div class="shop">
+        <Item itemID="1" price="$5" title="1 Month Discord Nitro"/>
+        <Item itemID="2" price="COMING SOON" title="1 Year Discord Nitro"/>
+    </div>
+</main>
+
+<style lang="css">
+
+body{
+    background: #34495e;
+    margin:0px;
+    padding:0px;
+    overflow:hidden;
+    color:#ecf0f1;
+    height:100vh;
+}
+
+h1{
+    margin:0px;
+    padding:0px;
+}
+
+.shop {  display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas:
+    ". . ."
+    ". . ."
+    ". . .";
+    place-items:center;
+    padding:25px;
+}
+
+.btn{
+    border:none;
+    color: #ecf0f1;
+    padding:10px;
+    border-radius:5px;
+    width: 5rem;
+    cursor: pointer;
+}
+
+.red{
+    background: #e74c3c;
+}
+
+.nav{
+    background:#2c3e50;
+    height: 4rem;
+    color:#ecf0f1;
+    padding:10px;
+}
+
+.inner-nav{
+    justify-content: space-between;
+    display:flex;
+    align-items:center;
+    width:100%;
+    height:100%;
+}
+
+</style>
+{/if}
